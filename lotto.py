e69e0f4fcc27d06ee8be13bdcee0e66d08e0e090
justin <radium.laboratories@gmail.com>
2019-05-27 16:52:40 +0800

    red+green: generating combinations using itertools

--- lotto.py
+++ lotto.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 class Lotto:
     """
     A Lotto class based on Steven Skiena's Algorithm Design Manual (Chapter 1)
@@ -19,15 +21,9 @@ class Lotto:
         return s
 
     def possible_winning_number_combinations(self):
-        c1 = frozenset([3,2,1])
-        c2 = frozenset([1,2,4])
-        c3 = frozenset([1,2,5])
-        c4 = frozenset([1,3,4])
-        c5 = frozenset([1,3,5])
-        c6 = frozenset([1,4,5])
-        c7 = frozenset([2,3,4])
-        c8 = frozenset([2,3,5])
-        c9 = frozenset([2,4,5])
-        c10 = frozenset([3,4,5])
-        return set([c1,c2,c3,c4,c5,c6,c7,c8,c9,c10])
-        
+        s = set()
+        for i in combinations(self._set, self._k):
+            # frozenset so that it is hashable and can be placed in a set
+            # set allows equality of elements without order, unlike tuples
+            s.add(frozenset(i))
+        return s

65400a62e76fb460cdd34a14b4ce68f8f4f4c35f
justin <radium.laboratories@gmail.com>
2019-05-27 17:50:49 +0800

    red+green: used combinations to implement ticket coverage

--- lotto.py
+++ lotto.py
@@ -29,4 +29,41 @@ class Lotto:
         return s
 
     def possibilities_fully_covered(self, ticket_set):
+        # generate hashmap of all possible winning combinations, setting each 
+        # combination as False (means this possibility is uncovered)
+        h = {}
+        for c in self.possible_winning_number_combinations():
+            h[c] = False
+
+        # for each ticket
+        # generate covered possibilities (k choose l)
+        for ticket in ticket_set:
+            covered = self._generate_covered_possibilities(ticket)
+            # mark each covered possibility in hashmap as True
+            for i in covered:
+                h[i] = True
+        for key,value in h.items():
+            if (value == False):
+                return False
         return True
+
+    def _generate_covered_possibilities(self, ticket):
+        # each ticket, e.g. {1,2,4} covers {1,2,*}, {1,4,*}, {2,4,*}
+        # or (k choose l) * ((n - l) choose (k - l)) possibilities
+        # e.g. {1,2,*} covers {1,2,3}, {1,2,4}, {1,2,5}
+        # {1,4,*} covers {1,4,2} (same as {1,2,4} above), {1,4,3}, {1,4,5}
+        # (2,4,*} covers {2,4,1} (overlap), {2,4,3}, {2,4,5}
+        covered = set()
+        for possible_winning_combination in combinations(ticket, self._l):
+            pwc = frozenset(possible_winning_combination)
+            # remove pwc from n
+            remaining_numbers = self._set - pwc
+
+            # generate permutations using this pwc
+            for n in remaining_numbers:
+                s = list(possible_winning_combination)
+                s.append(n)
+                # add permutations to covered set
+                covered.add(frozenset(s))
+        return covered
+

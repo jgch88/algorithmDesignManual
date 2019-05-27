0148613bbec450db37b96159c8faf6a7034a2655
justin <radium.laboratories@gmail.com>
2019-05-27 16:41:19 +0800

    red+green: display winning combinations using frozenset/set implementation

--- lotto.py
+++ lotto.py
@@ -7,14 +7,27 @@ class Lotto:
         self._k = k
         self._j = j
         self._l = l
-        self._set = self._generateNumbers();
+        self._set = self._generate_numbers();
 
     def numbers(self):
         return self._set
 
-    def _generateNumbers(self):
+    def _generate_numbers(self):
         s = set()
         for i in range(1, self._n + 1):
             s.add(i)
         return s
+
+    def possible_winning_number_combinations(self):
+        c1 = frozenset([3,2,1])
+        c2 = frozenset([1,2,4])
+        c3 = frozenset([1,2,5])
+        c4 = frozenset([1,3,4])
+        c5 = frozenset([1,3,5])
+        c6 = frozenset([1,4,5])
+        c7 = frozenset([2,3,4])
+        c8 = frozenset([2,3,5])
+        c9 = frozenset([2,4,5])
+        c10 = frozenset([3,4,5])
+        return set([c1,c2,c3,c4,c5,c6,c7,c8,c9,c10])
         
--- test_lotto.py
+++ test_lotto.py
@@ -12,3 +12,20 @@ class TestLotto(unittest.TestCase):
     def test_lotto_can_display_numbers(self):
         l = Lotto(n=5)
         self.assertEqual(l.numbers(), set([1,2,3,4,5]))
+
+    def test_lotto_can_display_all_possible_winning_number_combinations(self):
+        l = Lotto(n=5, k=3, j=3, l=2)
+        # since k = 3, we have (5 choose 3 = 10) possible combinations
+        c1 = frozenset([1,2,3])
+        c2 = frozenset([1,2,4])
+        c3 = frozenset([1,2,5])
+        c4 = frozenset([1,3,4])
+        c5 = frozenset([1,3,5])
+        c6 = frozenset([1,4,5])
+        c7 = frozenset([2,3,4])
+        c8 = frozenset([2,3,5])
+        c9 = frozenset([2,4,5])
+        c10 = frozenset([3,4,5])
+        self.assertEqual(l.possible_winning_number_combinations(), 
+            set([c1,c2,c3,c4,c5,c6,c7,c8,c9,c10]))
+
